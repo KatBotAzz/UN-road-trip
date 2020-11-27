aef5a560ea69830315e33e4a0ea2a4004c9c349f
Kat <katharinabotel@Katharinas-MacBook-Pro.local>
2020-11-27 15:55:19 +0000

    Commit

---
+++ Assets/ProgressBarTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ProgressBarTest: MonoBehaviour
+
+{
+    public ProgressBarPro barTest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //print(transform.position.y/10);
+      barTest.SetValue(transform.position.y/10);  
+    }
+}
